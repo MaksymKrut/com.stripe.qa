28fa170eea2215ce1ccf5ac18c7d4f12f955b0f2
Maksym Krutskykh <maximkrut@yahoo.com>
2016-10-15 17:44:39 +0200

    Fixed gitignore

--- support/env.js
+++ support/env.js
@@ -1,4 +1,4 @@
 exports.BASE_URL = "https://api.stripe.com/v1/"
 exports.BINFIND_URL = "https://binfind.com/"
-exports.API_TEST_KEY_SEC = ""
-exports.API_TEST_KEY_PUB = ""
+exports.API_TEST_KEY_SEC = "sk_test_PnPRFXE5w0yyxzWfVX4Jzopo"
+exports.API_TEST_KEY_PUB = "pk_test_jk9xKnTbIHg8V8qnH91aLdkJ"
